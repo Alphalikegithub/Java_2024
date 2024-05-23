57b97896c7ef83040134de3e6696fa8143bab6ce
Alphalikegitee <qbz3725460829@163.com>
2024-05-23 15:38:53 +0800

    开始学习数据结构

---
+++ part20-data_sructure/src/com/cskaoyan/javase/collection/MyCollection.java
@@ -0,0 +1,11 @@
+package com.cskaoyan.javase.collection;
+
+/**
+ * @author alpha
+ * @program: Java_2024
+ * @description: 集合
+ * @since 2024-05-23 15:36
+ **/
+
+public class MyCollection {
+}
---
+++ part20-data_sructure/src/com/cskaoyan/javase/linked/Demo.java
@@ -0,0 +1,11 @@
+package com.cskaoyan.javase.linked;
+
+/**
+ * @author alpha
+ * @program: Java_2024
+ * @description: 链表
+ * @since 2024-05-23 15:38
+ **/
+
+public class Demo {
+}
