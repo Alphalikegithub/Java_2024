89b3b10a5497413e6fc38137711a2b14e70a3516
Alphalikegitee <qbz3725460829@163.com>
2024-05-23 16:19:07 +0800

    数据结构之循环链表

---
+++ part20-data_sructure/src/com/cskaoyan/javase/linked/DemoLinked2.java
@@ -0,0 +1,51 @@
+package com.cskaoyan.javase.linked;
+
+/**
+ * @author alpha
+ * @program: Java_2024
+ * @description: 双向链表
+ * @since 2024-05-23 15:50
+ **/
+
+public class DemoLinked2 {
+    public static void main(String[] args) {
+        DBNode a = new DBNode("A", null, null);
+        DBNode b = new DBNode("B", a, null);
+        DBNode c = new DBNode("C", b, null);
+        DBNode d = new DBNode("D", c, null);
+
+        a.next = b;
+        b.next = c;
+        c.next = d;
+
+
+        //改造为循环链表
+
+
+        System.out.println(a);
+
+    }
+}
+
+class DBNode {
+    String value;
+    //前指针域
+    DBNode pre;
+    //后指针域
+    DBNode next;
+
+    public DBNode(String value, DBNode pre, DBNode next) {
+        this.value = value;
+        this.pre = pre;
+        this.next = next;
+    }
+
+    @Override
+    public String toString() {
+        return "DBNode{" +
+                "value='" + value + '\'' +
+//                ", pre=" + pre +
+                ", next=" + next +
+                '}';
+    }
+}
