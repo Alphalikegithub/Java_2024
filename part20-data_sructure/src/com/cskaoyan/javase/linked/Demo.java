0340aaba5e72326ca63ce66056d0f6a5a78b0419
Alphalikegitee <qbz3725460829@163.com>
2024-05-23 15:56:43 +0800

    数据结构之单链表

--- part20-data_sructure/src/com/cskaoyan/javase/linked/Demo.java
+++ part20-data_sructure/src/com/cskaoyan/javase/linked/Demo.java
@@ -8,4 +8,24 @@ package com.cskaoyan.javase.linked;
  **/
 
 public class Demo {
+    public static void main(String[] args) {
+        Person ff = new Person("爸爸");
+        Person f = new Person("爸爸");
+        Person m = new Person("我");
+        Person s = new Person("孩子");
+
+        ff.son = f;
+        f.son = m;
+        m.son = s;
+
+    }
+}
+
+class Person{
+    String name;
+    Person son;
+
+    public Person(String name) {
+        this.name = name;
+    }
 }
